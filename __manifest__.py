67f9d6ebe4c1ad58cd94f97f961dc6459705d16c
phdmabuze <pashokveok@gmail.com>
2024-03-27 22:07:25 +0300

    init

---
+++ __manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': "Telegram Management",
+    'version': '1.0',
+    'depends': ['base'],
+    'author': "prokopevp",
+    'category': 'Telegram',
+    'description': """
+    DESCRIPTION HERE Telegram Management
+    """
+}
